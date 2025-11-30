694c5d76674570518208ee96b6825d5e9a8d098e
than191017 <than191017@gmail.com>
2025-11-30 17:05:00 +0900

    Title: 물의 상태, Time: 121ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251130/물의 상태/state-of-water.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int n;
+        n = sc.nextInt();
+
+        if (n > 100){
+            System.out.println("vapor");
+            }
+            else if (n < 0){
+                System.out.println("ice");}
+            else {
+                System.out.println("water");
+        }
+
+
+    }
+}
