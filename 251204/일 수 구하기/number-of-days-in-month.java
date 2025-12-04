8c5f010cb6b0156226439525bd270620d37f710f
than191017 <than191017@gmail.com>
2025-12-04 21:21:25 +0900

    Title: 일 수 구하기, Time: 123ms, Memory: 10MB, Status: Passed - Codetree

--- 251204/일 수 구하기/number-of-days-in-month.java
+++ 251204/일 수 구하기/number-of-days-in-month.java
@@ -5,10 +5,14 @@ public class Main {
 
         int a = sc.nextInt();
 
-        if(a==2 || a==3 || a==5 || a==7 || a==8 || a==10 || a==12){
-            System.out.println("31");
-        }else{
-            System.out.println("30");
+        if(a==1 || a==3 || a==5 || a==7 || a==8 || a==10 || a==12){
+            System.out.println(31);
+        }else if(a==2)
+        {
+            System.out.println(28);
+        }
+        else{
+            System.out.println(30);
         }
     }
 }
