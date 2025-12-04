f114487adcb0746291552548698da0b8d9fb31ed
than191017 <than191017@gmail.com>
2025-12-04 21:30:14 +0900

    Title: 최댓값 구하기, Time: 124ms, Memory: 10MB, Status: Passed - Codetree

--- 251204/최댓값 구하기/maximum-value.java
+++ 251204/최댓값 구하기/maximum-value.java
@@ -11,8 +11,8 @@ public class Main {
             if (a > c) {
                 System.out.println(a);
             }
-            else if(b > c){
-                System.out.println(b);
+            else{
+                System.out.println(c);
             }
         }
         else if(b > c){
