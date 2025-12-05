560832405cb17f6127a2e044433cb5b8755fca83
than191017 <than191017@gmail.com>
2025-12-05 15:25:22 +0900

    Title: 10개 입력받기, Time: 142ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251205/10개 입력받기/receive-10-inputs.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // 정수 배열 선언
+        int[] arr = new int[10];
+        int cnt = 0;
+        int sum = 0;
+        float avg = 0;
+
+        // 10개의 정수를 배열에 입력받으며 0이 있는지 확인합니다.
+        for(int i = 0; i < 10; i++) {
+            arr[i] = sc.nextInt();
+            sum += (int)arr[i];
+            cnt++;
+            if(arr[i] == 0)
+                break;
+        }
+        avg = sum /(cnt-1);
+        System.out.print(sum + " ");
+        System.out.printf("%.1f",avg);
+    }
+}
