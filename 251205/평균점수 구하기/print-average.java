11819ceb760b3041c170d0437d44050e17c64d0a
than191017 <than191017@gmail.com>
2025-12-05 13:26:14 +0900

    Title: 평균점수 구하기, Time: 153ms, Memory: 12MB, Status: Passed - Codetree

---
+++ 251205/평균점수 구하기/print-average.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double[] arr = new double[8];
+        double sum = 0;
+        double avg = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextDouble();
+            sum += arr[i];
+        }
+
+        avg = sum/arr.length;
+
+        System.out.printf("%.1f",avg);
+    }
+}
