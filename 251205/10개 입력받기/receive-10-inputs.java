a9489f9997ad62c647895a3215c0ae5f2f650862
than191017 <than191017@gmail.com>
2025-12-05 15:29:40 +0900

    Title: 10개 입력받기, Time: 131ms, Memory: 10MB, Status: Passed - Codetree

--- 251205/10개 입력받기/receive-10-inputs.java
+++ 251205/10개 입력받기/receive-10-inputs.java
@@ -6,20 +6,23 @@ public class Main {
 
         // 정수 배열 선언
         int[] arr = new int[10];
-        int cnt = 0;
         int sum = 0;
+        int cnt = 0;
 
-
-        // 10개의 정수를 배열에 입력받으며 0이 있는지 확인합니다.
-        for(int i = 0; i < 10; i++) {
+        // 10개의 정수를 배열에 입력받으며 0이 있는지 확인합니다. 0이 나오지 않았을 경우 합과 개수를 구합니다.
+        for(int i = 0; i < 10; i++){
             arr[i] = sc.nextInt();
-            sum += (int)arr[i];
-            cnt++;
             if(arr[i] == 0)
                 break;
+            sum += arr[i];
+            cnt++;
         }
-        double avg = (double)(sum /(cnt-1));
-        System.out.print(sum + " ");
-        System.out.printf("%.1f",avg);
+
+        // 평균을 구합니다.
+        double avg = (double)sum / cnt;
+
+        //출력
+        System.out.printf("%d %.1f\n", sum, avg);
     }
 }
+
