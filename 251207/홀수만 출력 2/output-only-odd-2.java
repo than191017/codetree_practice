9fbdaf253b297728c4c4ce49ea0cbb23470afa44
than191017 <than191017@gmail.com>
2025-12-07 20:05:42 +0900

    Title: 홀수만 출력 2, Time: 128ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251207/홀수만 출력 2/output-only-odd-2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int b = sc.nextInt();
+        int a = sc.nextInt();
+
+        for (int i = b; i > (a-1); i--) {
+            if (i % 2 != 0){
+                System.out.print(i+ " ");
+            }
+            else if (i == a){
+                break;
+            }
+        }
+    }
+}
