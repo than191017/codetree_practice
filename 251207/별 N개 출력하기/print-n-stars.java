1dbce1a5cc4e13b04befbb173bf539caa3455677
than191017 <than191017@gmail.com>
2025-12-07 20:49:52 +0900

    Title: 별 N개 출력하기, Time: 125ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251207/별 N개 출력하기/print-n-stars.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();                 // 시작 값(초기화식)
+        int i = 0;
+
+        while (i < n) {           // 계속 반복할 조건(조건식)
+            System.out.println("*");  // 원하는 코드 작성
+            i++;                    // 원하는 증감값 설정(증감식)
+        }
+
+    }
+}
