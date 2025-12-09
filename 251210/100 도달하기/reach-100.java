ff85bda749541c13a8303decf60ccb9867c34194
than191017 <than191017@gmail.com>
2025-12-10 00:23:59 +0900

    Title: 100 도달하기, Time: 133ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 251210/100 도달하기/reach-100.java
+++ 251210/100 도달하기/reach-100.java
@@ -3,11 +3,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    int n = sc.nextInt();
-
     int arr[] = new int[100];
     arr[0] = 1;
-    arr[1] = n;
+    arr[1] = sc.nextInt();
     System.out.print(arr[0] + " ");
     System.out.print(arr[1] + " ");
 
