1b56ad8ed9d0cc2cab71df0d1473c336208c3e31
than191017 <than191017@gmail.com>
2025-12-10 00:51:35 +0900

    Title: 1-9 개수 세기, Time: 142ms, Memory: 10MB, Status: Passed - Codetree

--- 251210/1-9 개수 세기/count-one-to-nine.java
+++ 251210/1-9 개수 세기/count-one-to-nine.java
@@ -7,24 +7,19 @@ public class Main {
         int[] arr = new int[n];
         int[] countArr = new int[10];
 
+        // 입력 받기
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
 
         // 개수 세기
-        for (int i = 0; i < 10; i++) {
-            if (n > i){
+        for (int i = 0; i < n; i++) {
             countArr[arr[i]]++;
-            }
-            if(n <= (i+1)){
-                for (int j = 1; j < 10; j++) {
-                System.out.println(countArr[j]);
-                }
-                break;
-            }
-            
         }
 
-        // 개수 출력
+        // 개수 출력 (1부터 9까지)
+        for (int i = 1; i < 10; i++) {
+            System.out.println(countArr[i]);
+        }
     }
 }
