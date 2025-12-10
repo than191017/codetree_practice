147ddd08184e158e76b7c81703b0bf31898a6cda
than191017 <than191017@gmail.com>
2025-12-10 13:08:51 +0900

    Title: 나눗셈의 나머지, Time: 120ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 251210/나눗셈의 나머지/remainder-of-division.java
+++ 251210/나눗셈의 나머지/remainder-of-division.java
@@ -10,20 +10,16 @@ public class Main {
         int tmp = 0;
         int sum = 0;
 
-        int[] arr = new int[100];
+        int[] arr = new int[b];  // b 크기로 충분합니다 (나머지는 0~b-1)
 
-        for (int i = 0; i < 100; i++) {
-            tmp = (int) a % b;
-            a = (int) a / b;
-            arr[tmp] += 1;
-
-            if(a == 0){
-                break;
-            }
+        while (a >= 1) {  // a가 1 미만이 될 때까지
+            tmp = a % b;      // 먼저 나머지 구하기
+            arr[tmp] += 1;    // 나머지 카운트
+            a = a / b;        // 그 다음 몫 구하기
         }
 
-        for (int j = 0; j < 100; j++) {
-            sum += arr[j]*arr[j];
+        for (int j = 0; j < b; j++) {
+            sum += arr[j] * arr[j];
         }
 
         System.out.println(sum);
