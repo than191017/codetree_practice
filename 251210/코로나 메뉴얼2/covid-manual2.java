e029d67e9f18724d1a93ef3718bba2748171e2ed
than191017 <than191017@gmail.com>
2025-12-10 12:54:44 +0900

    Title: 코로나 메뉴얼2, Time: 134ms, Memory: 10MB, Status: Passed - Codetree

--- 251210/코로나 메뉴얼2/covid-manual2.java
+++ 251210/코로나 메뉴얼2/covid-manual2.java
@@ -3,34 +3,37 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] arr = new int[3];
-        String[] symptom  = new String[3];
 
-        int[] count = new int[5];
+        // 카운팅 배열의 1에는 A인 사람의 수가, 2에는 B가, 3에는 C가, 4에는 D가 들어감
+        int[] countArr = new int[5];
+        // type num = 분류 번호
+        int typeNum = 0;
+        char s;
+        int t;
 
-        for (int i = 0; i < 3; i++) {
-            symptom[i] = sc.next();
-            arr[i] = sc.nextInt();
-            if (symptom[i].equals("Y") && arr[i] >= 37 ){
-                count[1]++;
-            }
-            else if (symptom[i].equals("N") && arr[i]>= 37 ){
-                count[2]++;
-            }
-            else if (symptom[i].equals("Y") && arr[i] < 37 ){
-                count[3]++;
-            }
-            else {
-                count[4]++;
-            }
+        // s와 t를 입력받은 후 카운팅 배열을 통해 각각의 빈도 저장
+        for(int i = 0; i < 3; i++) {
+            s = sc.next().charAt(0);
+            t = sc.nextInt();
+
+            if(t >= 37 && s == 'Y')
+                typeNum = 1;
+            else if(t >= 37)
+                typeNum = 2;
+            else if(s == 'Y')
+                typeNum = 3;
+            else
+                typeNum = 4;
+            countArr[typeNum]++;
         }
 
-        for (int i = 1 ; i < 5; i++){
-            System.out.print(count[i] + " ");
+        // A부터 D까지 나온 횟수를 출력
+        for(int i = 1; i <= 4; i++) {
+            System.out.print(countArr[i] + " ");
         }
-        if (count[1] >= 2){
-            System.out.print("E")
-        }
-
+        
+        if(countArr[1] >= 2)
+            System.out.println("E");
     }
 }
+
