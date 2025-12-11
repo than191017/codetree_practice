52d7f340b661e5263baa11c2e5a736e34d36a074
than191017 <than191017@gmail.com>
2025-12-11 12:55:33 +0900

    Title: 2가 3번째로 등장하는 위치, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 251211/2가 3번째로 등장하는 위치/where-2-appears-3rd.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int cnt = 0;
+        int[] arr = new int[n];
+
+        for (int i = 0 i <n; i++){
+            arr[i] = sc.nextInt();
+            if (arr[i] == 2){
+                cnt ++;
+                if (cnt == 3){
+                    System.out.println(i);
+                    break;
+                }
+            }
+        }
+    }
+}
+
+>> 2
