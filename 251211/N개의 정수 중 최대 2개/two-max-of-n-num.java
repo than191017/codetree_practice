65a484e224e91a28404f1b507182e11d4ab2fd9d
than191017 <than191017@gmail.com>
2025-12-11 21:03:02 +0900

    Title: N개의 정수 중 최대 2개, Time: 121ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 251211/N개의 정수 중 최대 2개/two-max-of-n-num.java
+++ 251211/N개의 정수 중 최대 2개/two-max-of-n-num.java
@@ -1,29 +1,32 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] arr = new int[n];
-        int secondmax ;
-        int minVal = Integer.MAX_VALUE;
         
-        for (int i = 0; i < n; i++){
+        // 배열 입력받기
+        for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
-            secondmax = arr[i];
-
-            if (arr[i] < minVal){
-                minVal = arr[i];
-            }
-
-            if (i >= 1 && secondmax > arr[i-1] && secondmax >= minVal ){
-                secondmax = arr[i-1];
-            }
-
-
-
         }
-    
-        System.out.println(minVal + " " + secondmax);
+        
+        // 1번째와 2번째 최댓값 초기화
+        int max = Integer.MIN_VALUE;
+        int secondMax = Integer.MIN_VALUE;
+        
+        // 배열을 순회하며 최댓값 갱신
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > max) {
+                // 새로운 최댓값 발견
+                secondMax = max;  // 기존 max를 secondMax로
+                max = arr[i];      // 새로운 값을 max로
+            } else if (arr[i] > secondMax && arr[i] < max) {
+                // max보다 작지만 secondMax보다 큰 경우
+                secondMax = arr[i];
+            }
+        }
+        
+        System.out.println(max + " " + secondMax);
     }
 }
-
