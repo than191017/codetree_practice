042a050173adbc97c9877d0c96e8d0dbb333a5e6
than191017 <than191017@gmail.com>
2025-12-13 22:40:24 +0900

    Title: 500 근처의 수, Time: 113ms, Memory: 10MB, Status: Passed - Codetree

--- 251213/500 근처의 수/near-500.java
+++ 251213/500 근처의 수/near-500.java
@@ -5,30 +5,23 @@ public class Main {
 
         int[] arr = new int[10];
 
-        int max5 = 0;
-        int min5 = 0;
-        int totalMax = 0;
+        int max5 = 0;      // 500 미만 최댓값
+        int min5 = 1001;   // 500 초과 최솟값 (충분히 큰 값으로 초기화)
 
-        for (int i = 0 ; i < 10 ; i++){
+        for (int i = 0; i < 10; i++){
             arr[i] = sc.nextInt();
-            if (arr[i] > max5 && arr[i] < 500){
+            
+            // 500 미만 중 최댓값
+            if (arr[i] < 500 && arr[i] > max5){
                 max5 = arr[i];
             }
-            if (arr[i] > 500 & arr[i] > totalMax){
-                totalMax = arr[i];
-            }
-        }
-
-        for (int i = 0 ; i < 10 ; i++){
-            if (arr[i] > 500 && arr[i] < totalMax){
+            
+            // 500 초과 중 최솟값
+            if (arr[i] > 500 && arr[i] < min5){
                 min5 = arr[i];
             }
         }
 
-
-
-        System.out.print( max5 + " " + min5);
-
-
+        System.out.print(max5 + " " + min5);
     }
 }
