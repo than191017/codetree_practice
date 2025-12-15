f91231f2822231a3265c64e85dc9ec8e86749ff7
than191017 <than191017@gmail.com>
2025-12-15 23:21:57 +0900

    Title: 배열의 평균, Time: 137ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251215/배열의 평균/ave-of-array.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[][] arr2d = new int[2][4];
+        int horSum = 0;
+        int vertiSum = 0;
+        int allSum = 0;
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 4; j++) {
+                arr2d[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 4; j++) {
+                horSum += arr2d[i][j];
+            }
+            System.out.printf("%.1f" , (double)(horSum/4));
+            System.out.print(" ");
+            horSum = 0;
+        }
+        System.out.println();
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 2; j++) {
+                vertiSum += arr2d[j][i];
+            }
+            System.out.printf("%.1f" , (double)(vertiSum/2));
+            System.out.print(" ");
+            vertiSum = 0;
+        }
+        System.out.println();
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 4; j++) {
+                allSum += arr2d[i][j];
+            }
+        }
+        System.out.printf("%.1f" , (double)(allSum/8));
+
+
+
+    }
+}
+
